0ccfd3e6a8b2c94532e7274ae1938f91812929e5
Wasi623-tools <waseemasghar662323@gmail.com>
2025-05-14 22:08:11 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/useless-genius-emporium/',
+  
   server: {
     host: "::",
     port: 8080,
